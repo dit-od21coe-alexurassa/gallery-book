5b87c370757a08432898ccbd4b186603f9a0e2d3
Urassa <alexurassa03@gmail.com>
2024-07-09 17:42:20 +0300

    Add & configure bottom-nav, albums fragment & account fragment

--- app/src/main/java/com/example/gallerybook/MainActivity.java
+++ app/src/main/java/com/example/gallerybook/MainActivity.java
@@ -1,19 +1,48 @@
 package com.example.gallerybook;
 
+import android.accounts.Account;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    AlbumsFragment albumsFragment = new AlbumsFragment();
+    AccountFragment accountFragment = new AccountFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navbar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, albumsFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                final int itemId = item.getItemId();
+                if (itemId == R.id.menu_account) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, accountFragment).commit();
+                    return true;
+                }
+                if (itemId == R.id.menu_albums) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, albumsFragment).commit();
+                    return true;
+                }
+                return true;
+            }
+        });
     }
 }
